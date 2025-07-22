c57c7238dd9465d7a52cc1770a353714c62271b9
jtolong <j.tolong99@gmail.com>
2025-07-21 22:20:58 -0400

    teleporthq

---
+++ teleporthq/src/views/slide1699.js
@@ -0,0 +1,283 @@
+import React from 'react'
+
+import { Helmet } from 'react-helmet'
+
+import './slide1699.css'
+
+const Slide1699 = (props) => {
+  return (
+    <div className="slide1699-container">
+      <Helmet>
+        <title>exported project</title>
+      </Helmet>
+      <div className="slide1699-slide1699">
+        <img
+          alt="AdobeStock121534004221798"
+          src="/external/adobestock121534004221798-46xn-800w.png"
+          className="slide1699-adobe-stock12153400422"
+        />
+        <img
+          alt="Rectangle1956318"
+          src="/external/rectangle1956318-qgd4-200h.png"
+          className="slide1699-rectangle195"
+        />
+        <img
+          alt="Rectangle1945917"
+          src="/external/rectangle1945917-d83l-900h.png"
+          className="slide1699-rectangle194"
+        />
+        <span className="slide1699-text10">
+          “I wish I could just walk through my room before I buy anything.”
+        </span>
+        <span className="slide1699-text11">
+          Creative and constantly restyling her space, Mia is a design student
+          who wants easy, mobile tools to test ideas without stress or
+          commitment.
+        </span>
+        <span className="slide1699-text12">Bio</span>
+        <span className="slide1699-text13">
+          <span>Visualize layouts before committing</span>
+          <br></br>
+          <span>Customize room style on a budget</span>
+          <br></br>
+          <span>Easy, mobile-first design tools</span>
+        </span>
+        <span className="slide1699-text19">Wants &amp; Needs</span>
+        <span className="slide1699-text20">
+          <br></br>
+          <span>Items often don’t match or fit</span>
+          <br></br>
+          <span>Tools feel clunky or unrealistic</span>
+          <br></br>
+          <span>Struggles with limited space and rental rules</span>
+        </span>
+        <span className="slide1699-text27">Frustrations</span>
+        <div className="slide1699-group207">
+          <span className="slide1699-text28">Graphic Design Student</span>
+          <img
+            alt="wallet046423"
+            src="/external/wallet046423-zkua.svg"
+            className="slide1699-wallet04"
+          />
+        </div>
+        <div className="slide1699-group205">
+          <img
+            alt="home076421"
+            src="/external/home076421-4s77.svg"
+            className="slide1699-home07"
+          />
+          <span className="slide1699-text29">Single</span>
+        </div>
+        <div className="slide1699-group206">
+          <img
+            alt="location016422"
+            src="/external/location016422-1ln.svg"
+            className="slide1699-location01"
+          />
+          <span className="slide1699-text30">Toronto, Ontario</span>
+        </div>
+        <div className="slide1699-group208">
+          <img
+            alt="user6422"
+            src="/external/user6422-tdm.svg"
+            className="slide1699-user"
+          />
+          <span className="slide1699-text31">
+            24
+            <span
+              dangerouslySetInnerHTML={{
+                __html: ' ',
+              }}
+            />
+          </span>
+        </div>
+        <span className="slide1699-text32">
+          Mia Torres
+          <span
+            dangerouslySetInnerHTML={{
+              __html: ' ',
+            }}
+          />
+        </span>
+        <span className="slide1699-text33">Favorite Brands</span>
+        <img
+          alt="IKEALogowine6525"
+          src="/external/ikealogowine6525-hzc7-200h.png"
+          className="slide1699ikea-logowine"
+        />
+        <img
+          alt="muji6525"
+          src="/external/muji6525-ks9g-200h.png"
+          className="slide1699-muji"
+        />
+        <img
+          alt="UrbanOutfittersLogo6526"
+          src="/external/urbanoutfitterslogo6526-dv5c-200h.png"
+          className="slide1699-urban-outfitters-logo"
+        />
+        <img
+          alt="WayfairLogo6526"
+          src="/external/wayfairlogo6526-dxma-200h.png"
+          className="slide1699-wayfair-logo"
+        />
+        <span className="slide1699-text34">
+          <span>Internet</span>
+          <br></br>
+          <span>Social Media</span>
+          <br></br>
+          <span>Online Shopping</span>
+          <br></br>
+          <span>
+            Gadgets
+            <span
+              dangerouslySetInnerHTML={{
+                __html: ' ',
+              }}
+            />
+          </span>
+          <br></br>
+          <span>Early Adapter</span>
+        </span>
+        <div className="slide1699-group209">
+          <img
+            alt="Ellipse386526"
+            src="/external/ellipse386526-q2ea-200h.png"
+            className="slide1699-ellipse381"
+          />
+          <img
+            alt="Ellipse396526"
+            src="/external/ellipse396526-qt3q-200h.png"
+            className="slide1699-ellipse391"
+          />
+          <img
+            alt="Ellipse406526"
+            src="/external/ellipse406526-7d0j-200h.png"
+            className="slide1699-ellipse401"
+          />
+          <img
+            alt="Ellipse416526"
+            src="/external/ellipse416526-969-200h.png"
+            className="slide1699-ellipse411"
+          />
+          <img
+            alt="Ellipse426526"
+            src="/external/ellipse426526-ykk7-200h.png"
+            className="slide1699-ellipse421"
+          />
+        </div>
+        <div className="slide1699-group210">
+          <img
+            alt="Ellipse386527"
+            src="/external/ellipse386527-rdtk-200h.png"
+            className="slide1699-ellipse382"
+          />
+          <img
+            alt="Ellipse396527"
+            src="/external/ellipse396527-p0h-200h.png"
+            className="slide1699-ellipse392"
+          />
+          <img
+            alt="Ellipse406527"
+            src="/external/ellipse406527-c0u-200h.png"
+            className="slide1699-ellipse402"
+          />
+          <img
+            alt="Ellipse416527"
+            src="/external/ellipse416527-tao-200h.png"
+            className="slide1699-ellipse412"
+          />
+          <img
+            alt="Ellipse426527"
+            src="/external/ellipse426527-7jss-200h.png"
+            className="slide1699-ellipse422"
+          />
+        </div>
+        <div className="slide1699-group211">
+          <img
+            alt="Ellipse386527"
+            src="/external/ellipse386527-fmo-200h.png"
+            className="slide1699-ellipse383"
+          />
+          <img
+            alt="Ellipse396527"
+            src="/external/ellipse396527-yt4a-200h.png"
+            className="slide1699-ellipse393"
+          />
+          <img
+            alt="Ellipse406527"
+            src="/external/ellipse406527-k5l-200h.png"
+            className="slide1699-ellipse403"
+          />
+          <img
+            alt="Ellipse416527"
+            src="/external/ellipse416527-oam-200h.png"
+            className="slide1699-ellipse413"
+          />
+          <img
+            alt="Ellipse426528"
+            src="/external/ellipse426528-xkvt-200h.png"
+            className="slide1699-ellipse423"
+          />
+        </div>
+        <div className="slide1699-group212">
+          <img
+            alt="Ellipse386528"
+            src="/external/ellipse386528-e1g-200h.png"
+            className="slide1699-ellipse384"
+          />
+          <img
+            alt="Ellipse396528"
+            src="/external/ellipse396528-2nol-200h.png"
+            className="slide1699-ellipse394"
+          />
+          <img
+            alt="Ellipse406528"
+            src="/external/ellipse406528-xp0n-200h.png"
+            className="slide1699-ellipse404"
+          />
+          <img
+            alt="Ellipse416528"
+            src="/external/ellipse416528-1k3i-200h.png"
+            className="slide1699-ellipse414"
+          />
+          <img
+            alt="Ellipse426528"
+            src="/external/ellipse426528-pwqb-200h.png"
+            className="slide1699-ellipse424"
+          />
+        </div>
+        <div className="slide1699-group213">
+          <img
+            alt="Ellipse386528"
+            src="/external/ellipse386528-54jg-200h.png"
+            className="slide1699-ellipse385"
+          />
+          <img
+            alt="Ellipse396528"
+            src="/external/ellipse396528-7qvk-200h.png"
+            className="slide1699-ellipse395"
+          />
+          <img
+            alt="Ellipse406529"
+            src="/external/ellipse406529-wve-200h.png"
+            className="slide1699-ellipse405"
+          />
+          <img
+            alt="Ellipse416529"
+            src="/external/ellipse416529-rw5y-200h.png"
+            className="slide1699-ellipse415"
+          />
+          <img
+            alt="Ellipse426529"
+            src="/external/ellipse426529-o5xn-200h.png"
+            className="slide1699-ellipse425"
+          />
+        </div>
+        <span className="slide1699-text44">Journey Map.</span>
+        <span className="slide1699-text45">Ideate.</span>
+      </div>
+    </div>
+  )
+}
+
+export default Slide1699
